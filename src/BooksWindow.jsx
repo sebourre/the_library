7d0f2807ad054df4ca590f23fcba7b14cdaeaf35
sebourre <s3bourre@gmail.com>
2026-01-26 19:24:01 +0100

    Working on Adjustments

--- src/BooksWindow.jsx
+++ src/BooksWindow.jsx
@@ -3,6 +3,7 @@ import './BooksWindow.css';
 
 export default function BooksWindow({booksWindowRef, gamesCount, moviesCount, seriesCount}){
   const [sort, setSort] = useState('digits');
+  const [booksAdjustments, setBooksAdjustments] = useState(false);
   const [booksAdditional, setBooksAdditional] = useState(false);
 
   return(
@@ -46,6 +47,7 @@ export default function BooksWindow({booksWindowRef, gamesCount, moviesCount, se
         </div>
         <div>
           <svg
+            onClick={() => setBooksAdjustments(!booksAdjustments)}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="none"
@@ -71,6 +73,11 @@ export default function BooksWindow({booksWindowRef, gamesCount, moviesCount, se
             <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
             <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
           </svg>
+        </div>
+        <div className='books_adjustments' style={{display: booksAdjustments ? 'flex' : 'none'}}>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
         <div className='books_additional' style={{display: booksAdditional ? 'flex' : 'none'}}>
           <svg
