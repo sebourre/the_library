dd301a738e1c887324880fd7481aedf224cf9cc0
sebourre <s3bourre@gmail.com>
2026-02-14 16:45:23 +0100

    Added button Search on Google

--- src/CardWindow.jsx
+++ src/CardWindow.jsx
@@ -113,6 +113,22 @@ export default function CardWindow({cardWindowRef, displayCardWindow, cardBookma
           </div>
         </div>
         <div className='card_window_log'>Logged on {cardLog}</div>
+        <svg
+          className='card_window_search'
+          onClick={() => window.open(`https://www.google.com/search?q=${cardTitle}`, '_blank')}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="rgba(245, 245, 245, 0.7)"
+        >
+          <path d="M21 12a9 9 0 1 0 -9 9" />
+          <path d="M3.6 9h16.8" />
+          <path d="M3.6 15h7.9" />
+          <path d="M11.5 3a17 17 0 0 0 0 18" />
+          <path d="M12.5 3a16.984 16.984 0 0 1 2.574 8.62" />
+          <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M20.2 20.2l1.8 1.8" />
+        </svg>
       </div>
     </div>
   )
