7aab44ee7ef361411ad87356cbe2d904e514bf82
sebourre <s3bourre@gmail.com>
2026-02-24 10:44:36 +0100

    Added and improved some animations

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -44,9 +44,10 @@ export default function SearchBar({searchCards, resetSearchValue}){
   return(
     <div className='search_bar' title='Search' onClick={searchFocus}>
       <svg
+        style={isSearchFocused ? {animation: 'none'} : null}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        fill="none"
+        fill={isSearchFocused ? 'var(--white-hue)' : 'none'}
         stroke="var(--white-hue)"
       >
         <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
