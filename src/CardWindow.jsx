e2ad5c93fba8d9e521f6efd76bbfe87a63f2070d
sebourre <s3bourre@gmail.com>
2026-03-05 09:12:28 +0100

    Improved card window bar when maximized

--- src/CardWindow.jsx
+++ src/CardWindow.jsx
@@ -130,16 +130,21 @@ export default function CardWindow({cardWindowRef, displayCardWindow, cardBookma
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="var(--white-hue)">
               <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/>
             </svg>
-            <b>{maximize ? 'Rating:' : null}</b> {cardRating}
+            <b>{maximize ? 'Rating:' : null}</b> {maximize ? null : cardRating}
           </p>
-          <div className='card_window_bar'>
+          <div className={maximize ? 'card_window_bar card_window_bar_maximized' : 'card_window_bar'}>
             <div 
-              className='card_window_point' 
-              style={{
+              className={maximize ? 'card_window_point card_window_point_maximized' : 'card_window_point'}
+              style={maximize ? {
+                left: (100 - (cardRating == 100 ? 90.5 : cardRating > 93 ? 93.5 : cardRating < 5 ? 4.5 : cardRating)) + '%',
+                transform: `translate(-${cardRating}%, -50%)`
+              } : {
                 left: (100 - cardRating) + '%',
                 transform: `translate(-${cardRating}%, -50%)`
               }}
-            ></div>
+            >
+              {maximize ? cardRating : null}
+            </div>
           </div>
         </div>
         <div className='card_window_log'>Logged on {cardLog}</div>
