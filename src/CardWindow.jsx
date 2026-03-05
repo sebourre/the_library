88b275cbf8dde0e2b4795db3a64f16b7682f3b18
sebourre <s3bourre@gmail.com>
2026-03-05 09:33:45 +0100

    Still working on card maximized

--- src/CardWindow.jsx
+++ src/CardWindow.jsx
@@ -100,7 +100,7 @@ export default function CardWindow({cardWindowRef, displayCardWindow, cardBookma
             {maximize ? cardType : null}
           </div>
         </div>
-        <div className='card_window_footer'>
+        <div className='card_window_info'>
           <p title={cardMaker}>
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="var(--white-hue)" >
               <path d="m15 12-9.373 9.373a1 1 0 0 1-3.001-3L12 9"/>
@@ -125,7 +125,8 @@ export default function CardWindow({cardWindowRef, displayCardWindow, cardBookma
             </svg>
             <b>{maximize ? 'Date of release:' : null}</b> {cardDate ? trueDate(cardDate) : '-'}
           </p>
-          <br />
+        </div>
+        <div className='card_window_footer'>
           <p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="var(--white-hue)">
               <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/>
