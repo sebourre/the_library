b38118e1fc9dd34d2a4d836b8a1d52f3e4ca0fd1
sebourre <s3bourre@gmail.com>
2026-03-11 09:38:30 +0100

    Working on button Import image

--- src/LogWindow.jsx
+++ src/LogWindow.jsx
@@ -67,6 +67,18 @@ export default function LogWindow({setLogWindowOn, displayLogWindow, logWindowOn
           <legend>Image</legend>
           <input type="url" name='image' onChange={updateImagePreview} placeholder='URL' />
         </fieldset>
+        <svg 
+          className='image_import'
+          xmlns="http://www.w3.org/2000/svg" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="var(--white-hue)" 
+        >
+          <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/>
+          <path d="m14 19 3 3v-5.5"/>
+          <path d="m17 22 3-3"/>
+          <circle cx="9" cy="9" r="2"/>
+        </svg>
         <div className='image_preview'>
           <img src={imagePreview} />
         </div>
